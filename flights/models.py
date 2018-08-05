d491dfbff6da5e6485de35a44b1b919df3b1a3d6
alexHooper <alexmohack@gmail.com>
2018-08-05 18:04:54 +0530

    add method for departed and absolute url

--- flights/models.py
+++ flights/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from datetime import datetime
+from datetime import datetime, timedelta
+from django.urls import reverse
 
 current_time = datetime.now()
 
@@ -7,7 +8,7 @@ class Flight(models.Model):
 	origin = models.CharField(max_length=50)
 	destination = models.CharField(max_length=50)
 	price = models.DecimalField(decimal_places=2, max_digits=10000)
-	departure = models.DateTimeField()
+	departure = models.DateTimeField(default=current_time + timedelta(hours=2))
 	duration = models.DurationField(
 		help_text="Duration for flight in hh:mm:ss format"
 	)
@@ -24,3 +25,12 @@ class Flight(models.Model):
 
 	def __str__(self):
 		return f"Flight: {self.origin} to {self.destination}"
+
+	@property
+	def has_departed(self):
+		if self.departure > current_time.now():
+			return True
+		return False
+
+	def get_absolute_url(self):
+		return reverse("flight_detail", args=[str(self.id)])
