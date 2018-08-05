04f2699c700fdbc9cc97bfcd65cf517856ab14fc
alexHooper <alexmohack@gmail.com>
2018-08-05 14:39:48 +0530

    change path for template

--- flights/views.py
+++ flights/views.py
@@ -5,4 +5,4 @@ from .models import Flight
 def flights_list(request):
 	flights_list = Flight.objects.all()
 	context = {'flights_list': flights_list}
-	return render(request, 'flights_list.html', context)
+	return render(request, 'flights/flights_list.html', context)
