b22fbdc5c3d094e514f226396aaa7d06da87a142
alexHooper <alexmohack@gmail.com>
2018-08-05 14:43:05 +0530

    change the url for flights_list view

--- flights/urls.py
+++ flights/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import flights_list
 
 urlpatterns = [
-	path('list/', flights_list, name='flights_list'),
+	path('flights-list/', flights_list, name='flights_list'),
 ]
