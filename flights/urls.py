5570ae85bb83a78a96791d1efb9e70467f386ebc
alexHooper <alexmohack@gmail.com>
2018-08-05 17:42:11 +0530

    add url for home page and flight detail

--- flights/urls.py
+++ flights/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import flights_list
+from .views import flights_list, flight_detail, home_page
 
 urlpatterns = [
 	path('flights/list/', flights_list, name='flights_list'),
+	path("flight/<int:id>/detail/", flight_detail, name="flight_detail"),
+	path("", home_page, name="home_page"),
 ]
