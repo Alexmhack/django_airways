cb1cdab8b3ac97fcecef51fa5f72c0c92532f579
alexHooper <alexmohack@gmail.com>
2018-08-05 15:05:36 +0530

    add folder for static files

--- flights_app/settings.py
+++ flights_app/settings.py
@@ -119,3 +119,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
