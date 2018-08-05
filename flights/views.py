f76e022948084eed2a4f7a047bafeb5e335c0dde
alexHooper <alexmohack@gmail.com>
2018-08-05 16:38:51 +0530

    change list view and add detail view

--- flights/views.py
+++ flights/views.py
@@ -3,6 +3,18 @@ from django.shortcuts import render
 from .models import Flight
 
 def flights_list(request):
-	flights_list = Flight.objects.all()
-	context = {'flights_list': flights_list}
+	flights_list = Flight.objects.filter(status__exact="a")
+	context = {
+		'flights_list': flights_list,
+	}
+	
 	return render(request, 'flights/flights_list.html', context)
+
+
+def flight_detail(request, pk):
+	flight = Flight.objects.get(pk=pk)
+	context = {
+		'flight': flight
+	}
+
+	return render(request, "flights/flight_detail.html", context)
