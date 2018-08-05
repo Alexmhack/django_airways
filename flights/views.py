aaa4256031d3a99b2a71a05f52f735ee3c7daad4
alexHooper <alexmohack@gmail.com>
2018-08-05 17:42:45 +0530

    add view for home page and change pk to id

--- flights/views.py
+++ flights/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 from .models import Flight
 
+def home_page(request):
+	latest_flights = Flight.objects.filter(status__exact="a")[:6]
+	return render(request, "flights/home_page.html", context={'latest_flights': latest_flights})
+
+
 def flights_list(request):
 	flights_list = Flight.objects.filter(status__exact="a")
 	context = {
@@ -11,8 +16,8 @@ def flights_list(request):
 	return render(request, 'flights/flights_list.html', context)
 
 
-def flight_detail(request, pk):
-	flight = Flight.objects.get(pk=pk)
+def flight_detail(request, id):
+	flight = Flight.objects.get(pk=id)
 	context = {
 		'flight': flight
 	}
