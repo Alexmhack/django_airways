9f2ed797a40a09f4a77ac2f187c57f3a1bff59e8
alexHooper <alexmohack@gmail.com>
2018-08-06 00:47:41 +0530

    add url for our contact form

--- flights/urls.py
+++ flights/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import flights_list, flight_detail, home_page, search_results_view
+from .views import (
+	flights_list,
+	flight_detail,
+	home_page,
+	search_results_view,
+	contact_view
+)
 
 urlpatterns = [
 	path('flights/list/', flights_list, name='flights_list'),
 	path("flight/<int:id>/detail/", flight_detail, name="flight_detail"),
 	path("", home_page, name="home_page"),
 	path("search/results/", search_results_view, name="search_results"),
+	path("contact/", contact_view, name="contact_form"),
 ]
