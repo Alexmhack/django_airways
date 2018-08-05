ccd884d37b1b7706f82f0d01d1859bbc72186a63
alexHooper <alexmohack@gmail.com>
2018-08-05 14:29:13 +0530

    add search field in admin site

--- flights/admin.py
+++ flights/admin.py
@@ -5,3 +5,4 @@ from .models import Flight
 @admin.register(Flight)
 class FlightAdmin(admin.ModelAdmin):
 	list_display = ('origin', 'destination', 'duration', 'status')
+	search_fields = ('origin', 'destination', 'duration', 'status')
