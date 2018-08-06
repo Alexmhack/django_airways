cb2904eafa6992b4f2a48ac232736c1a8914149f
alexHooper <alexmohack@gmail.com>
2018-08-06 15:33:46 +0530

    add flights CRUD url and use class based views

--- flights/urls.py
+++ flights/urls.py
@@ -2,16 +2,24 @@ from django.urls import path
 
 from .views import (
 	FlightListView,
-	flight_detail,
-	home_page,
+	FlightDetailView,
+	HomePageView,
 	search_results_view,
-	contact_view
+	contact_view,
+	FlightCreateView
 )
 
 urlpatterns = [
 	path('flights/list/', FlightListView.as_view(), name='flights_list'),
-	path("flight/<int:id>/detail/", flight_detail, name="flight_detail"),
-	path("", home_page, name="home_page"),
+	path("flight/<int:pk>/detail/", FlightDetailView.as_view(), name="flight_detail"),
+	path("", HomePageView.as_view(), name="home_page"),
 	path("search/results/", search_results_view, name="search_results"),
 	path("contact/", contact_view, name="contact_form"),
+]
+
+# Flight CRUD urls
+urlpatterns += [
+	path('flight/create/', FlightCreateView.as_view(), name="flight-create"),
+	path('flight/<int:pk>/update/', FlightUpdateView.as_view(), name="flight-update"),
+	path('flight/<int:pk>/delete/', FlightDeleteView.as_view(), name="flight-delete"),
 ]
