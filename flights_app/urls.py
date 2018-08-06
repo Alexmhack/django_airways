e4329e60bbfbaf5605bcd959f21c2cdc9f0643ae
alexHooper <alexmohack@gmail.com>
2018-08-07 01:09:26 +0530

    include urls for passengers app

--- flights_app/urls.py
+++ flights_app/urls.py
@@ -19,8 +19,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('flights.urls')),
+    path('passengers/', include('passengers.urls'))
 ]
 
 urlpatterns += [
-	path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
