c457631dc2a4f1da8e68f828379fa4575844ec62
alexHooper <alexmohack@gmail.com>
2018-08-06 16:52:40 +0530

    create our contact form

--- flights/forms.py
+++ flights/forms.py
@@ -1,4 +1,13 @@
 from django import forms
 
 class ContactForm(forms.Form):
-	
+	contact_name = forms.CharField(required=True)
+	contact_email = forms.EmailField(required=True)
+	content = forms.CharField(
+		required=True,
+		widget=forms.Textarea)
+
+	def __init__(self, *args, **kwargs):
+		super(ContactForm, self).__init__(*args, **kwargs)
+		self.fields['contact_name'].label = 'Your name:'
+		self.fields['contact_email'].label = 'Your email:'
