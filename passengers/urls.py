1ee19db669a1308a338b550c841f461d5a545fb3
alexHooper <alexmohack@gmail.com>
2018-08-07 02:32:37 +0530

    configure urls for creating and deleting

--- passengers/urls.py
+++ passengers/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
 	path('list/', PassengerList.as_view(), name="passenger-list"),
 	path('<int:pk>/detail/', PassengerDetailView.as_view(), name="passenger-detail"),
 	path('create/', PassengerCreateView.as_view(), name="passenger-create"),
-	path('delete/', PassengerDeleteView.as_view(), name="passenger-delete"),
-	path('update/', PassengerUpdateView.as_view(), name="passenger-update"),
+	path('delete/<int:pk>', PassengerDeleteView.as_view(), name="passenger-delete"),
+	path('<int:pk>/update/', PassengerUpdateView.as_view(), name="passenger-update"),
 ]
