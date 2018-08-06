30dedd8c2e009e32d7491197e671fa0438e1bbb3
alexHooper <alexmohack@gmail.com>
2018-08-07 02:11:04 +0530

    add urls for our crud opertions

--- passengers/urls.py
+++ passengers/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
 from .views import (
-	PassengerList
+	PassengerList,
+	PassengerDetailView,
+	PassengerCreateView,
+	PassengerDeleteView,
+	PassengerUpdateView,
 )
 
 urlpatterns = [
 	path('list/', PassengerList.as_view(), name="passenger-list"),
+	path('<int:pk>/detail/', PassengerDetailView.as_view(), name="passenger-detail"),
+	path('create/', PassengerCreateView.as_view(), name="passenger-create"),
+	path('delete/', PassengerDeleteView.as_view(), name="passenger-delete"),
+	path('update/', PassengerUpdateView.as_view(), name="passenger-update"),
 ]
