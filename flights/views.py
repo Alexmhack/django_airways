6882969b4c6edc85c486711e2cef7e2b262ed040
alexHooper <alexmohack@gmail.com>
2018-08-06 16:56:11 +0530

    add logic for using our form and sending email

--- flights/views.py
+++ flights/views.py
@@ -4,8 +4,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect
+from django.template.loader import get_template
 
 from .models import Flight
+from .forms import ContactForm, NotifyForm
 
 from datetime import datetime
 
@@ -39,24 +43,40 @@ def search_results_view(request):
 		"query": query
 	}
 	return render(request, "flights/search_results.html", context)
-
+	
 
 def contact_view(request):
+	contact_form = ContactForm
+
 	if request.method == "POST":
-		try:
-			contact_name = request.POST.get("name")
-			contact_email = request.POST.get("email")
+		form = contact_form(data=request.POST)
+
+		if form.is_valid():
+			contact_name = request.POST.get('contact_name', '')
+			contact_email = request.POST.get('contact_email', '')
+			form_content = request.POST.get('content', '')
+
+			template = get_template('contact_template.txt')
 			context = {
-				"contact_name": contact_name,
-				"contact_email": contact_email
+				'contact_name': contact_name,
+				'contact_email': contact_email,
+				'form_content': form_content
 			}
 
-			return render(request, "flights/contact_form_submit.html", context)
-		except Exception as e:
-			error = e
-			return render(request, "flights/contact_form_submit.html", {'error': error})
-	else:
-		return render(request, "flights/contact_form.html")
+			content = template.render(context)
+
+			email = EmailMessage(
+				"New Contact Form Submission",
+				content,
+				"Your Django App ",
+				["pranavg456@gmail.com"],
+				headers={'Reply-To': contact_email}
+			)
+
+			email.send()
+			return render(request, 'contact_confirm.html')
+
+	return render(request, 'contact_form.html', {'contact_form': contact_form})
 
 
 # Flights CRUD
