f3ac28bbd46ec55374f4d9d7ac53fdb48302689c
alexHooper <alexmohack@gmail.com>
2018-08-06 14:50:26 +0530

    add admin class for our Passenger model

--- passengers/admin.py
+++ passengers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Passenger
+
+@admin.register(Passenger)
+class Passenger(admin.ModelAdmin):
+	list_display = ('first_name', 'last_name', 'flight')
+	search_fields = ("first_name", "last_name", "flight")
