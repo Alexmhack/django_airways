7235c78a315054b79d7d9cb34482f40b35b50aef
alexHooper <alexmohack@gmail.com>
2018-08-07 01:11:02 +0530

    add class based view for passenger list

--- passengers/views.py
+++ passengers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Passenger
+
+class PassengerList(LoginRequiredMixin, generic.ListView):
+	model = Passenger
+	template_name = "passengers/passenger_list.html"
