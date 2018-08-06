5e5850abc544393f9f27761522b87b5ad92eed37
alexHooper <alexmohack@gmail.com>
2018-08-06 19:31:41 +0530

    delete the whole app for some migrations issues

--- passengers/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Passenger
-
-@admin.register(Passenger)
-class Passenger(admin.ModelAdmin):
-	list_display = ('first_name', 'last_name', 'flight')
-	search_fields = ("first_name", "last_name", "flight")
