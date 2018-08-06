98a0dd9edf3722c0292f50b6300bc38e3bbe3c16
alexHooper <alexmohack@gmail.com>
2018-08-06 14:51:09 +0530

    add model for passengers

--- passengers/models.py
+++ passengers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from flights.models import Flight
+
+class Passenger(models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	email = models.EmailField()
+	flight = models.ForeignKey(Flight, help_text="Select a flight for the passenger", on_delete=models.CASCADE)
+
+	def __str__(self):
+		return f"{self.first_name} {self.last_name} is travelling from {self.flight}"
