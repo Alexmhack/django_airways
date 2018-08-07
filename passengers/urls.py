5ef66620a95479ef4753801bef3b524544f8a36c
alexHooper <alexmohack@gmail.com>
2018-08-07 20:40:27 +0530

    importing views from passengers app

--- passengers/urls.py
+++ passengers/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
 
-from flights import views as core_views
+from passengers import views as core_views
 
 from .views import (
 	PassengerList,
