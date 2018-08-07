249fc331453cb1319423498fab0cba0294ffdd6e
alexHooper <alexmohack@gmail.com>
2018-08-07 21:11:12 +0530

    register our profile model

--- passengers/admin.py
+++ passengers/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import Passenger
+from .models import Passenger, Profile
 
 @admin.register(Passenger)
 class PassengerAdmin(admin.ModelAdmin):
 	list_display = ('passenger', 'flight')
 	search_fields = ('passenger', 'flight')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ('user', 'bio', 'email_confirm')
+	search_fields = ('user')
