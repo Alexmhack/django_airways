5c91edf825abeeaca8985b1952abca515c509081
alexHooper <alexmohack@gmail.com>
2018-08-07 21:00:09 +0530

    change the urls to path

--- passengers/urls.py
+++ passengers/urls.py
@@ -25,7 +25,6 @@ urlpatterns += [
 ]
 
 urlpatterns += [
-	url(r'^account_activation_sent/$', core_views.account_activation_sent, name='account_activation_sent'),
-	url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-		core_views.activate, name='activate'),
+	path('account/activation/sent/', views.account_activation_sent, name="account_activation_sent"),
+	path('accounts/reset/<uidb64>/<token>/', views.activate, name="activate"),
 ]
