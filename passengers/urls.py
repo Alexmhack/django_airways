05d2540788768570e56f109c7831c65dd8426d50
alexHooper <alexmohack@gmail.com>
2018-08-07 20:30:08 +0530

    correct error by replacing core with views

--- passengers/urls.py
+++ passengers/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
 
-from flights.core import views as core_views
+from flights import views as core_views
 
 from .views import (
 	PassengerList,
@@ -25,7 +25,7 @@ urlpatterns += [
 ]
 
 urlpatterns += [
-    url(r'^account_activation_sent/$', core_views.account_activation_sent, name='account_activation_sent'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        core_views.activate, name='activate'),
+	url(r'^account_activation_sent/$', core_views.account_activation_sent, name='account_activation_sent'),
+	url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+		core_views.activate, name='activate'),
 ]
