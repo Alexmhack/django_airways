15eb2f7e4dc519a9ce93d263a952a12c1e1f9e2b
alexHooper <alexmohack@gmail.com>
2018-08-07 20:19:36 +0530

    view that user will be redirect to by clicking the link in email

--- passengers/views.py
+++ passengers/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
+from django.utils.encoding import force_bytes, force_text
 
 from .models import Passenger
 from . forms import SignupForm
@@ -76,3 +76,20 @@ def signup_view(request):
 	else:
 		form = SignupForm()
 	return render(request, "registration/signup.html", {'form': form})
+
+
+def activation(request, uidb64, token):
+	try:
+		uid = force_text(urlsafe_base64_decode(uidb64))
+		user = User.objects.get(pk=uid)
+	except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+		user = None
+
+	if user is not None and account_activation_token.check_token(user, token):
+		user.is_active = True
+		user.profile.email_confirmed = True
+		user.save()
+		login(request, user)
+		return redirect('home')
+	else:
+		return render(request, 'account_activation_invalid.html')
