29936f3de89179126a0c6111cfeb8ad3ce07a335
alexHooper <alexmohack@gmail.com>
2018-08-07 21:11:48 +0530

    add validation methods

--- passengers/forms.py
+++ passengers/forms.py
@@ -10,3 +10,34 @@ class SignupForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+	def clean_username(self):
+		username = self.cleaned_data['username'].lower()
+		r = User.objects.filter(username=username)
+		if r.count():
+			raise ValidationError("Username already exists")
+		return username
+
+	def clean_email(self):
+		email = self.cleaned_data['email'].lower()
+		r = User.objects.filter(email=email)
+		if r.count():
+			raise ValidationError("Email already exists")
+		return email
+
+	def clean_password2(self):
+		password1 = self.cleaned_data.get('password1')
+		password2 = self.cleaned_data.get('password2')
+
+		if password1 and password2 and password1 != password2:
+			raise ValidationError("Password don't match")
+
+		return password2
+
+	def save(self, commit=True):
+		user = User.objects.create_user(
+			self.cleaned_data['username'],
+			self.cleaned_data['email'],
+			self.cleaned_data['password1']
+		)
+		return use
