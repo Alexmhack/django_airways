98eeaa148d267a56e256fba48fe6739045f22f2d
alexHooper <alexmohack@gmail.com>
2018-08-14 23:49:24 +0530

    print info for error detection

--- passengers/views.py
+++ passengers/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_text
 
 from .models import Passenger
@@ -68,10 +69,7 @@ def signup_view(request):
 				"token": account_activation_token.make_token(user)
 			})
 
-			username = form.cleaned_data.get('username')
-			raw_password = form.cleaned_data.get('password1')
-			user = authenticate(request, username=username, password=raw_password)
-			login(request, user)
+			user.email_user(subject, message)
 			return redirect("account_activation_sent")
 	else:
 		form = SignupForm()
@@ -82,8 +80,17 @@ def activate(request, uidb64, token):
 	try:
 		uid = force_text(urlsafe_base64_decode(uidb64))
 		user = User.objects.get(pk=uid)
+		print(user)
 	except (TypeError, ValueError, OverflowError, User.DoesNotExist):
 		user = None
+
+	if account_activation_token.check_token(user, token):
+		print('CHECKED')
+	else:
+		print('WRONG VALUE')
+		print(token)
+		print(user)
+		print(uidb64)
 
 	if user is not None and account_activation_token.check_token(user, token):
 		user.is_active = True
@@ -92,8 +99,8 @@ def activate(request, uidb64, token):
 		login(request, user)
 		return redirect('flights_list')
 	else:
-		return render(request, 'account_activation_invalid.html')
+		return render(request, 'registration/account_activation_invalid.html')
 
 
 def account_activation_sent(request):
-	render(request, "registration/account_activation_sent.html")
+	return render(request, "registration/account_activation_sent.html")
