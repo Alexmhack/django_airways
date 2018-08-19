f0574f4cea5da02a42f883f3224680ce690cfd32
PSG <35827095+Alexmhack@users.noreply.github.com>
2018-08-19 20:40:06 +0530

    remove wrong password and email details

--- flights_app/settings.py
+++ flights_app/settings.py
@@ -134,8 +134,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '78030psg@gmail.com'
-EMAIL_HOST_PASSWORD = "thisisauselessemail"
+EMAIL_HOST_USER = 'ghost@protocol.com'
+EMAIL_HOST_PASSWORD = "fallout"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_SUBJECT_PREFIX = "Django-Webapp"
