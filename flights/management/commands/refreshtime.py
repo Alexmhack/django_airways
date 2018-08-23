c7fc5489dacba47eb12a4544da96fbebb5969ca2
alexHooper <alexmohack@gmail.com>
2018-08-23 10:19:16 +0530

    add django-admin command for refreshing departure time

---
+++ flights/management/commands/refreshtime.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from flights.models import Flight
+
+class Command(BaseCommand):
+    help = 'Resfreshes all Flights departure time'
+
+    def add_arguments(self, parser):
+        parser.add_argument('flights', type=int)
+
+    def handle(self, *args, **options):
+        print(options)
+        return Flight.objects.refresh_departure_time(items=options['flights'])
